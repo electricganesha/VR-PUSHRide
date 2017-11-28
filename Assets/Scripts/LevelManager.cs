e48c56ab48cb3718209efb1ee53288873c51fb17
Christian Marques <christian.marques@gmail.com>
2017-11-28 11:37:37 +0000

    PUSH VR Ride 1.0

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    bool hasEndBeenReached = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(hasEndBeenReached)
+        {
+            if (Input.GetButtonUp("Tap"))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+
+	}
+
+    public void EndHasBeenReached()
+    {
+        hasEndBeenReached = true;
+    }
+}
